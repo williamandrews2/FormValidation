370d0d5a2aec9201ec9560023dac13df85c3e7f0
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-04-15 21:45:28 -0500

    Added password validation logic, reworked functions, styling changes.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
 const submitButton = document.getElementById("submit-button");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+let confirmPasswordTouched = false; // TODO reset when/if form resets.
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,13 +14,27 @@ form.addEventListener("submit", (event) => {
   }
 });
 
+confirmPassword.addEventListener("input", () => {
+  confirmPasswordTouched = true;
+  checkPasswords();
+});
+
+confirmPassword.addEventListener("blur", () => {
+  confirmPasswordTouched = true;
+});
+
+password.addEventListener("input", () => {
+  checkPasswords();
+});
+
 inputs.forEach((input) => {
+  if (input === confirmPassword) return;
+
   const errorSpan = input.nextElementSibling;
 
-  input.addEventListener("blur", (event) => {
+  input.addEventListener("blur", () => {
     if (input.validity.valid) {
-      errorSpan.textContent = "";
-      errorSpan.className = "error";
+      clearError(errorSpan);
     } else {
       showError(input, errorSpan);
     }
@@ -31,9 +48,30 @@ function showError(input, errorSpan) {
     errorSpan.textContent = `You must enter a valid ${input.name || input.id}.`;
   } else if (input.validity.patternMismatch) {
     errorSpan.textContent = `Invalid format for ${input.name || input.id}.`;
+  } else if (input.validity.customError) {
+    errorSpan.textContent = input.validationMessage;
   }
 
-  // LEFT OFF HERE, ISSUES WITH TEXTCONTENT FOR ALL EXCEPT EMAIL. CHECK VALIDATION
-
   errorSpan.className = "error active";
+}
+
+function checkPasswords() {
+  // using confirmPassword as we only want the error to show up on the confirm password input field.
+  const errorSpan = confirmPassword.nextElementSibling;
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords do not match");
+
+    // prevent error message from rendering prematurely
+    if (confirmPasswordTouched) {
+      showError(confirmPassword, errorSpan);
+    }
+  } else {
+    confirmPassword.setCustomValidity("");
+    clearError(errorSpan);
+  }
+}
+
+function clearError(errorSpan) {
+  errorSpan.textContent = "";
+  errorSpan.className = "error";
 }
